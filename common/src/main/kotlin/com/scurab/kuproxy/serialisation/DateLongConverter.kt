702cbc65c1bf378a755eb36e3e1dda2fda695870
Jiri Bruchanov <jbruchanov@gmail.com>
2021-04-27 19:57:18 +0100

    Add data conversion

---
+++ common/src/main/kotlin/com/scurab/kuproxy/serialisation/DateLongConverter.kt
@@ -0,0 +1,11 @@
+package com.scurab.kuproxy.serialisation
+
+import java.text.SimpleDateFormat
+
+
+open class DateLongConverter(
+    private val converter: SimpleDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+) {
+    open fun convert(item: Long): String = converter.format(item)
+    open fun convert(item: String): Long = converter.parse(item).time
+}
