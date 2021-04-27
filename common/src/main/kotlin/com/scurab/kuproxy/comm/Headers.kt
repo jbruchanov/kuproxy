d59b44fa9c1240fc92c6995c139a1c8bd202f65f
Jiri Bruchanov <jbruchanov@gmail.com>
2021-04-27 19:19:13 +0100

    Add Headers

--- common/src/main/kotlin/com/scurab/kuproxy/comm/Headers.kt
+++ common/src/main/kotlin/com/scurab/kuproxy/comm/Headers.kt
@@ -2,5 +2,6 @@ package com.scurab.kuproxy.comm
 
 object Headers {
     const val ContentType = "content-type"
+    const val ContentLength = "content-length"
     const val AcceptEncoding = "accept-encoding"
 }
